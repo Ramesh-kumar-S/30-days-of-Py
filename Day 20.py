176edc4738d5cf40049545f73eb60d555eb628c8
Ramesh Kumar S <75178176+Ramesh-kumar-S@users.noreply.github.com>
2021-09-28 00:40:35 +0530

    Update Day 20.py

--- Day 20.py
+++ Day 20.py
@@ -1,12 +1,25 @@
-# a=[4,3,1,2]
-a=[3,2,1]
-# a=[1,2,3]
-swaps=0
-for i in range(len(a)):
-#     swaps=0
-    for j in range(len(a)-1):
-        if a[j]>a[j+1]:
-            a[j],a[j+1]=a[j+1],a[j]
-            swaps+=1
-            print(a)
-print(swaps)
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+    
+    swaps=0
+    for i in range(n):
+        for j in range(n-1):
+            if a[j]>a[j+1]:
+                a[j],a[j+1]=a[j+1],a[j]
+                swaps+=1
+    print(f'Array is sorted in {swaps} swaps.')
+    print(f'First Element: {a[0]}')
+    print(f'Last Element: {a[-1]}')
+    # Write your code here
